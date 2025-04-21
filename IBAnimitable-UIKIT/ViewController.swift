06e0f305a05702ab61bf020ef110071a2f241a75
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-21 18:30:57 +0530

    Xcode Updated Changes

--- IBAnimitable-UIKIT/ViewController.swift
+++ IBAnimitable-UIKIT/ViewController.swift
@@ -9,20 +9,33 @@ import UIKit
 import IBAnimatable
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var animatedView: AnimatableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-//        animatedView.animate(.pop(repeatCount: 1))
-        animatedView.animate(.squeezeFade(way: .in, direction: .up))
-        animatedView.animate(.squeeze(way: .in, direction: .right), duration: 1, damping: 1, velocity: 2, force: 1)
-
-
+        //        animatedView.animate(.pop(repeatCount: 1))
+        //        animatedView.animate(.squeezeFade(way: .in, direction: .up))
+        //        animatedView.animate(.squeeze(way: .in, direction: .right), duration: 1, damping: 1, velocity: 2, force: 1)
+        
+        
+        //        animatedView.animate(.squeezeFade(way: .in, direction: .down))
+        //            .then(.pop(repeatCount: 1))
+        //            .then(.shake(repeatCount: 1))
+        //            .then(.squeeze(way: .in, direction: .down))
+        //            .then(.wobble(repeatCount: 1))
+        //            .then(.flip(along: .x))
+        //            .then(.flip(along: .y))
+        //            .then(.slideFade(way: .out, direction: .down))
+        //
+        //
+        //    }
+        
+        
+        animatedView.animate(.compound(animations: .init(arrayLiteral: .pop(repeatCount: 4)), run: .parallel), velocity: 0)
+        
     }
-
-
+    
 }
-
